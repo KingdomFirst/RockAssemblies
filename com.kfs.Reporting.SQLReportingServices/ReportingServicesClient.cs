d83bf04da683ca54c766ff5252621b724c95b807
Chris Funk <chrisf@kingdomfirstsolutions.com>
2017-02-22 18:01:23 -0500

    Reporting Services Code Library

---
+++ com.kfs.Reporting.SQLReportingServices/ReportingServicesClient.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace com.kfs.Reporting.SQLReportingServices
+{
+    public class ReportingServicesClient
+    {
+        #region Properties
+        public string ServerUrl  { get; set; }
+        public string ReportPath { get; set; }
+        public string ContentManagerUser { get; set; }
+        public string ContentManagerPassword { get; set; }
+        public string BrowserUser { get; set; }
+        public string BrowserPassword { get; set; }
+        public bool Configured { get; set; }
+
+        private Guid ReportingServicesCategoryGuid = new Guid( "BE54A3EB-98F9-4BBE-86FD-A3F503CDADF6" );
+        #endregion
+
+        #region Constructors
+        public ReportingServicesClient()
+        {
+            LoadCredentials();
+        }
+        #endregion
+
+        #region Public Methods
+        public bool SaveCredentials(out string message)
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool TestConnection(out string message)
+        {
+            throw new NotImplementedException();
+        }
+        #endregion
+
+        #region Private Methods
+
+        private void LoadCredentials()
+        {
+            throw new NotImplementedException();
+        }
+
+        private bool ReportingServicesCategoryExists()
+        {
+            throw new NotImplementedException();
+        }
+
+        #endregion
+
+
+    }
+}
