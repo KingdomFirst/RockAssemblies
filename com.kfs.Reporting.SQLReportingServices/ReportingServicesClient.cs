d01c4ed18453db22b90c1a25546573a145a4f8e0
Chris Funk <chrisf@kingdomfirstsolutions.com>
2017-02-24 10:35:33 -0500

    Reporting Services Configuration Updates

--- com.kfs.Reporting.SQLReportingServices/ReportingServicesClient.cs
+++ com.kfs.Reporting.SQLReportingServices/ReportingServicesClient.cs
@@ -4,12 +4,16 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
+using Rock.Data;
+using Rock.Model;
+using Rock.Web.Cache;
+
 namespace com.kfs.Reporting.SQLReportingServices
 {
     public class ReportingServicesClient
     {
         #region Properties
-        public string ServerUrl  { get; set; }
+        public string ServerUrl { get; set; }
         public string ReportPath { get; set; }
         public string ContentManagerUser { get; set; }
         public string ContentManagerPassword { get; set; }
@@ -18,37 +22,269 @@ namespace com.kfs.Reporting.SQLReportingServices
         public bool Configured { get; set; }
 
         private Guid ReportingServicesCategoryGuid = new Guid( "BE54A3EB-98F9-4BBE-86FD-A3F503CDADF6" );
+        private const string SERVER_URL_KEY = "ReportingServiceURL";
+        private const string SERVER_ROOT_PATH_KEY = "ReportingServiceRootPath";
+        private const string CONTENT_MANAGER_USER_KEY = "ReportingServiceContentManagerUser";
+        private const string CONTENT_MANAGER_PWD_KEY = "ReportingServiceContentMangerPwd";
+        private const string BROWSER_USER_KEY = "ReportingServiceBrowserUser";
+        private const string BROWSER_PWD_KEY = "ReportingServiceBrowserPwd";
         #endregion
 
         #region Constructors
         public ReportingServicesClient()
         {
-            LoadCredentials();
+            LoadCredentials( new RockContext() );
         }
         #endregion
 
         #region Public Methods
-        public bool SaveCredentials(out string message)
+        public bool SaveCredentials( out string message )
         {
-            throw new NotImplementedException();
+            if ( !TestConnection( out message ) )
+            {
+                return false;
+            }
+
+            using ( RockContext context = new RockContext() )
+            {
+                int categoryId = GetReportingServicesCategory( context );
+                if ( categoryId <= 0 )
+                {
+                    categoryId = CreateReportingServicesCategory( context );
+                }
+
+                VerifyAttributes( context, categoryId );
+
+                SaveAttributeValue( context, SERVER_URL_KEY, ServerUrl );
+                SaveAttributeValue( context, SERVER_ROOT_PATH_KEY, ReportPath );
+                SaveAttributeValue( context, CONTENT_MANAGER_USER_KEY, ContentManagerUser );
+                SaveAttributeValue( context, CONTENT_MANAGER_PWD_KEY, ContentManagerPassword );
+                SaveAttributeValue( context, BROWSER_USER_KEY, BrowserUser );
+                SaveAttributeValue( context, BROWSER_PWD_KEY, BrowserPassword );
+
+
+            }
+
+            return true;
+
         }
 
-        public bool TestConnection(out string message)
+        public bool TestConnection( out string message )
         {
-            throw new NotImplementedException();
+            message = string.Empty;
+            return true;
+
         }
         #endregion
 
         #region Private Methods
 
-        private void LoadCredentials()
+        private void ClearProperties()
         {
-            throw new NotImplementedException();
+            ServerUrl = null;
+            ReportPath = null;
+            ContentManagerUser = null;
+            ContentManagerPassword = null;
+            BrowserUser = null;
+            BrowserPassword = null;
         }
 
-        private bool ReportingServicesCategoryExists()
+        private void LoadCredentials( RockContext context )
         {
-            throw new NotImplementedException();
+
+            ClearProperties();
+            GlobalAttributesCache cache = GlobalAttributesCache.Read();
+
+            ServerUrl = cache.GetValue( SERVER_URL_KEY, context );
+            ReportPath = cache.GetValue( SERVER_ROOT_PATH_KEY, context );
+            ContentManagerUser = cache.GetValue( CONTENT_MANAGER_USER_KEY, context );
+            ContentManagerPassword = cache.GetValue( CONTENT_MANAGER_PWD_KEY, context );
+            BrowserUser = cache.GetValue( BROWSER_USER_KEY, context );
+            BrowserPassword = cache.GetValue( BROWSER_PWD_KEY, context );
+
+        }
+
+        private int CreateReportingServicesCategory( RockContext context )
+        {
+            var service = new CategoryService( context );
+            Category category = new Category();
+            category.EntityTypeId = EntityTypeCache.Read( typeof( Rock.Model.Attribute ) ).Id;
+            category.EntityTypeQualifierColumn = "EntityTypeId";
+
+            category.Order = new CategoryService( context ).Queryable()
+                .Where( c => c.EntityTypeId == category.EntityTypeId )
+                .Where( c => c.EntityTypeQualifierColumn == category.EntityTypeQualifierColumn )
+                .OrderByDescending( c => c.Order )
+                .Select( c => c.Order )
+                .FirstOrDefault() + 1;
+            category.Name = "Reporting Services";
+            category.Guid = ReportingServicesCategoryGuid;
+            category.Description = "Reporting Service configuration Settings";
+            service.Add( category );
+            context.SaveChanges();
+
+            return category.Id;
+
+        }
+
+        private int GetReportingServicesCategory( RockContext context )
+        {
+            Category cat = new CategoryService( context ).Get( ReportingServicesCategoryGuid );
+
+
+            if ( cat == null )
+            {
+                return -1;
+            }
+            else
+            {
+                return cat.Id;
+            }
+        }
+
+        private void SaveAttributeValue( RockContext context, string key, string value )
+        {
+            var attributeSvc = new AttributeService( context );
+            var attribute = attributeSvc.GetGlobalAttribute( key );
+            var attributeValueSvc = new AttributeValueService( context );
+            var attributeValue = attributeValueSvc.GetByAttributeIdAndEntityId( attribute.Id, null );
+
+
+            if ( attributeValue == null && !String.IsNullOrWhiteSpace( value ) )
+            {
+                attributeValue = new AttributeValue();
+                attributeValue.AttributeId = attribute.Id;
+                attributeValue.EntityId = null;
+                attributeValueSvc.Add( attributeValue );
+            }
+
+            if ( attributeValue == null || value.Equals( attributeValue.Value ) )
+            {
+                return;
+            }
+
+            if ( String.IsNullOrWhiteSpace( value ) )
+            {
+                attributeValueSvc.Delete( attributeValue );
+            }
+            else
+            {
+                attributeValue.Value = value;
+            }
+
+            context.SaveChanges();
+        }
+
+        private void VerifyAttributes( RockContext context, int categoryId )
+        {
+            var attributeService = new AttributeService( context );
+            var category = new CategoryService( context ).Get( categoryId );
+            bool hasChanges = false;
+
+            var serverUrl = attributeService.GetGlobalAttribute( SERVER_URL_KEY );
+            var serverRootPath = attributeService.GetGlobalAttribute( SERVER_ROOT_PATH_KEY );
+            var contentManagerUser = attributeService.GetGlobalAttribute( CONTENT_MANAGER_USER_KEY );
+            var contentManagerPwd = attributeService.GetGlobalAttribute( CONTENT_MANAGER_PWD_KEY );
+            var browserUser = attributeService.GetGlobalAttribute( BROWSER_USER_KEY );
+            var browserPwd = attributeService.GetGlobalAttribute( BROWSER_PWD_KEY );
+
+            if ( serverUrl == null )
+            {
+
+                serverUrl = new Rock.Model.Attribute();
+                serverUrl.FieldTypeId = FieldTypeCache.Read( Rock.SystemGuid.FieldType.URL_LINK ).Id;
+                serverUrl.IsSystem = false;
+                serverUrl.Name = "Reporting Service URL";
+                serverUrl.Description = "URL to the SQL Reporting Services Reporting Server endpoint.";
+                serverUrl.Key = SERVER_URL_KEY;
+                serverUrl.IsRequired = false;
+                serverUrl.AllowSearch = false;
+                serverUrl.Categories.Add( category );
+                attributeService.Add( serverUrl );
+                hasChanges = true;
+            }
+
+            if ( serverRootPath == null )
+            {
+                serverRootPath = new Rock.Model.Attribute();
+                serverRootPath.FieldTypeId = FieldTypeCache.Read( Rock.SystemGuid.FieldType.TEXT ).Id;
+                serverRootPath.IsSystem = false;
+                serverRootPath.Name = "Reporting Service Root Folder.";
+                serverRootPath.Key = SERVER_ROOT_PATH_KEY;
+                serverRootPath.Description = "Root/Base folder for Rock reports in reporting services.";
+                serverRootPath.IsRequired = false;
+                serverRootPath.AllowSearch = false;
+                serverRootPath.Categories.Add( category );
+                attributeService.Add( serverRootPath );
+                hasChanges = true;
+            }
+
+            if ( contentManagerUser == null )
+            {
+                contentManagerUser = new Rock.Model.Attribute();
+                contentManagerUser.FieldTypeId = FieldTypeCache.Read( Rock.SystemGuid.FieldType.TEXT ).Id;
+                contentManagerUser.Name = "Reporting Service - Content Manager Username";
+                contentManagerUser.Key = CONTENT_MANAGER_USER_KEY;
+                contentManagerUser.Description = "The Reporting Server Content Manager (Report Administrator) User Name. (i.e. domain\\user format)";
+                contentManagerUser.IsRequired = false;
+                contentManagerUser.AllowSearch = false;
+                contentManagerUser.Categories.Add( category );
+                attributeService.Add( contentManagerUser );
+                hasChanges = true;
+            }
+
+            if ( contentManagerPwd == null )
+            {
+                contentManagerPwd = new Rock.Model.Attribute();
+                contentManagerPwd.FieldTypeId = FieldTypeCache.Read( Rock.SystemGuid.FieldType.ENCRYPTED_TEXT ).Id;
+                contentManagerPwd.Name = "Reporting Service - Content Manager Password";
+                contentManagerPwd.Key = CONTENT_MANAGER_PWD_KEY;
+                contentManagerPwd.Description = "The Content Manager Password.";
+                contentManagerPwd.IsRequired = false;
+                contentManagerPwd.AllowSearch = false;
+                contentManagerPwd.Categories.Add( category );
+                contentManagerPwd.AttributeQualifiers.Add( new AttributeQualifier { IsSystem = false, Key = "ispassword", Value = bool.TrueString } );
+                attributeService.Add( contentManagerPwd );
+                hasChanges = true;
+
+
+            }
+
+            if ( browserUser == null )
+            {
+                browserUser = new Rock.Model.Attribute();
+                browserUser.FieldTypeId = FieldTypeCache.Read( Rock.SystemGuid.FieldType.TEXT ).Id;
+                browserUser.Name = "Reporting Service - Browser User";
+                browserUser.Key = BROWSER_USER_KEY;
+                browserUser.Description = "The Reporting Server Browser (Report Viewer) User Name. (i.e. domain\\user format)";
+                browserUser.IsRequired = false;
+                browserUser.AllowSearch = false;
+                browserUser.Categories.Add( category );
+                attributeService.Add( browserUser );
+                hasChanges = true;
+            }
+
+            if ( browserPwd == null )
+            {
+                browserPwd = new Rock.Model.Attribute();
+                browserPwd.FieldTypeId = FieldTypeCache.Read( Rock.SystemGuid.FieldType.ENCRYPTED_TEXT ).Id;
+                browserPwd.Name = "Reporting Service - Browser Password";
+                browserPwd.Key = BROWSER_PWD_KEY;
+                browserPwd.Description = "The Reporting Server Browser Password.";
+                browserPwd.IsRequired = false;
+                browserPwd.AllowSearch = false;
+                browserPwd.Categories.Add( category );
+                browserPwd.AttributeQualifiers.Add( new AttributeQualifier { IsSystem = false, Key = "ispassword", Value = bool.TrueString } );
+                attributeService.Add( browserPwd );
+                hasChanges = true;
+            }
+
+            if ( hasChanges )
+            {
+                context.SaveChanges();
+                GlobalAttributesCache.Flush();
+            }
+
         }
 
         #endregion
