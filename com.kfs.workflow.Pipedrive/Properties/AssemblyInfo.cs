304fd72c8ebfb5e04b9ceea49f57ce5b53c6438c
treyhendon <treyhendon@gmail.com>
2017-05-31 12:45:51 -0400

    Standardized Assembly Properites

--- com.kfs.workflow.Pipedrive/Properties/AssemblyInfo.cs
+++ com.kfs.workflow.Pipedrive/Properties/AssemblyInfo.cs
@@ -8,7 +8,7 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyTitle( "KFS Pipedrive Actions" )]
 [assembly: AssemblyDescription( "" )]
 [assembly: AssemblyConfiguration( "" )]
-[assembly: AssemblyCompany( "" )]
+[assembly: AssemblyCompany( "Kingdom First Solutions" )]
 [assembly: AssemblyProduct( "KFS Pipedrive Actions" )]
 [assembly: AssemblyCopyright( "Copyright ©  2017" )]
 [assembly: AssemblyTrademark( "" )]
