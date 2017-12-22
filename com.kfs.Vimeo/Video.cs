abcd010105e1ec73271494f677fd727a194057f8
Trey Hendon <treyh@kingdomfirstsolutions.com>
2017-12-22 00:42:39 -0500

    Added first version of Vimeo sync assembly

---
+++ com.kfs.Vimeo/Video.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using VimeoDotNet;
+
+namespace com.kfs.Vimeo
+{
+    public class Video
+    {
+        /// <summary>
+        /// Returns the link of a picture having the closest width.
+        /// </summary>
+        /// <param name="client">The VimeoClient to use.</param>
+        /// <param name="videoId">The Vimeo Id of the video.</param>
+        /// <param name="width">The desired width of the picture.</param>
+        public string GetPicture( VimeoClient client, long videoId, int width = 1920 )
+        {
+            var link = string.Empty;
+
+            var pictureList = new List<VimeoDotNet.Models.Picture>();
+            var task = Task.Run( async () =>
+            {
+                var pictures = await client.GetPicturesAsync( videoId );
+                pictureList = pictures.data.ToList();
+            } );
+            task.Wait();
+            if ( pictureList.Count > 0 )
+            {
+                var picSizes = new List<VimeoDotNet.Models.Size>();
+                picSizes = pictureList.FirstOrDefault().sizes.ToList();
+                link = picSizes.Aggregate( ( x, y ) => Math.Abs( x.width - width ) < Math.Abs( y.width - width ) ? x : y ).link;
+            }
+            return link;
+        }
+
+        /// <summary>
+        /// Returns a VideoInfo object with video info.
+        /// </summary>
+        /// <param name="client">The VimeoClient to use.</param>
+        /// <param name="videoId">The Vimeo Id of the video.</param>
+        public VideoInfo GetVideoInfo( VimeoClient client, long videoId )
+        {
+            var videoInfo = new VideoInfo();
+
+            var task = Task.Run( async () =>
+            {
+                var video = await client.GetVideoAsync( videoId );
+                videoInfo.vimeoId = videoId;
+                videoInfo.name = video.name;
+                videoInfo.description = string.Format( "<p>{0}</p>", video.description.Replace( "\n", "<br>" ) );
+                videoInfo.duration = video.duration;
+                videoInfo.hdLink = video.HighDefinitionVideoSecureLink;
+                videoInfo.sdLink = video.StandardVideoSecureLink;
+                videoInfo.hlsLink = video.StreamingVideoSecureLink;
+            } );
+            task.Wait();
+
+            return videoInfo;
+        }
+
+        /// <summary>
+        /// An object to hold the links to the video files.
+        /// </summary>
+        public class VideoInfo
+        {
+            public long vimeoId;
+            public string name;
+            public string description;
+            public int duration;
+            public string hdLink;
+            public string sdLink;
+            public string hlsLink;
+        }
+    }
+}
