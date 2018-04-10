6eeebc420c0dedb249c83f7decd5c57be1f277a2
Trey Hendon <treyh@kingdomfirstsolutions.com>
2018-04-10 11:56:06 -0400

    Addded missing person attribute and workflow settings.

--- com.kfs.Checkr/Migrations/001_AddSystemData.cs
+++ com.kfs.Checkr/Migrations/001_AddSystemData.cs
@@ -5,6 +5,7 @@ using System.Text;
 using System.Threading.Tasks;
 
 using Rock.Plugin;
+using Rock.SystemGuid;
 
 namespace com.kfs.Checkr.Migrations
 {
@@ -24,6 +25,8 @@ namespace com.kfs.Checkr.Migrations
             RockMigrationHelper.AddDefinedValue( SystemGuid.DefinedType.CHECKR_PACKAGES, "tasker_basic", "Tasker Basic", "9cdb61e7-8083-4a6f-9606-c12cd24aaa10", false );
             RockMigrationHelper.AddDefinedValue( SystemGuid.DefinedType.CHECKR_PACKAGES, "employment_verification_package", "Employment Verification Package", "f5dfe151-dc3b-44e5-8e55-cdd52a7ec0cb", false );
             RockMigrationHelper.AddDefinedValue( SystemGuid.DefinedType.CHECKR_PACKAGES, "education_package", "Education Package", "b54dc0c4-bd34-4c91-bd1c-c70c3c482350", false );
+
+            RockMigrationHelper.UpdatePersonAttribute( Rock.SystemGuid.FieldType.URL_LINK, "4D1E1EBA-ABF2-4A7C-8ADF-65CB5AAE94E2", "Checkr Candidate Link", "CheckrCandidateLink", "", "", 6, "", "72e7649e-04c1-4787-adf9-69fc12971bb0" );
         }
 
         /// <summary>
