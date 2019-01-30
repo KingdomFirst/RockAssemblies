d00b0a33daf615aa3d5e0b904c62c8d050cd2589
Trey Hendon <treyh@kingdomfirstsolutions.com>
2019-01-30 16:55:02 -0500

    Updated projects for auto versioning

--- com.kfs.GroupScheduledSMS/Properties/AssemblyInfo.cs
+++ com.kfs.GroupScheduledSMS/Properties/AssemblyInfo.cs
@@ -1,36 +1,18 @@
 ﻿using System.Reflection;
-using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
 
 // General Information about an assembly is controlled through the following
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("com.kfs.GroupScheduledSMS")]
-[assembly: AssemblyDescription("")]
-[assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("com.kfs.GroupScheduledSMS")]
-[assembly: AssemblyCopyright("Copyright ©  2018")]
-[assembly: AssemblyTrademark("")]
-[assembly: AssemblyCulture("")]
+[assembly: AssemblyTitle( "com.kfs.GroupScheduledSMS" )]
+
+// Auto increment assembly versions
+[assembly: AssemblyVersion( "1.2.*" )]
 
 // Setting ComVisible to false makes the types in this assembly not visible
 // to COM components.  If you need to access a type in this assembly from
 // COM, set the ComVisible attribute to true on that type.
-[assembly: ComVisible(false)]
+[assembly: ComVisible( false )]
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("c575517c-dfce-45ae-a2de-3d4759b3825c")]
-
-// Version information for an assembly consists of the following four values:
-//
-//      Major Version
-//      Minor Version
-//      Build Number
-//      Revision
-//
-// You can specify all the values or you can default the Build and Revision Numbers
-// by using the '*' as shown below:
-// [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.1.*")]
-[assembly: AssemblyFileVersion("1.1")]
+[assembly: Guid( "c575517c-dfce-45ae-a2de-3d4759b3825c" )]
