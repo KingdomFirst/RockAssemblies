563a915855f413f6984da17029a4a2e721aebd0a
Trey Hendon <treyh@kingdomfirstsolutions.com>
2019-03-04 15:35:10 -0500

    Inactivate Groups Job

---
+++ rocks.kfs.InactivateGroups/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+﻿using System.Reflection;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle( "rocks.kfs.InactivateGroups" )]
+[assembly: AssemblyProduct( "rocks.kfs.InactivateGroups" )]
+[assembly: AssemblyCopyright( "Copyright © Kingdom First Solutions 2019" )]
+
+// Auto increment assembly versions
+[assembly: AssemblyVersion( "1.1.*" )]
+
+// Setting ComVisible to false makes the types in this assembly not visible
+// to COM components.  If you need to access a type in this assembly from
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible( false )]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid( "7de9e0a3-21ce-40ad-9d16-2a345298c3b7" )]
