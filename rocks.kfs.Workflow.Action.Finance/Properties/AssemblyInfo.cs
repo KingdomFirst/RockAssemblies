bc29af12bd25c2b68b7a45fa19fd7619c23f7e14
Trey Hendon <treyh@kingdomfirstsolutions.com>
2019-05-08 22:30:09 -0400

    Added financials workflow assembly with transaction entry action

---
+++ rocks.kfs.Workflow.Action.Finance/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+﻿using System.Reflection;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle( "rocks.kfs.Workflow.Action.Finance" )]
+[assembly: AssemblyProduct( "rocks.kfs.Workflow.Action.Finance" )]
+[assembly: AssemblyCopyright( "Copyright © Kingdom First Solutions 2019" )]
+
+// Auto increment assembly versions
+[assembly: AssemblyVersion( "1.0.*" )]
+
+// Setting ComVisible to false makes the types in this assembly not visible
+// to COM components.  If you need to access a type in this assembly from
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible( false )]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid( "4b374d69-08d2-48cc-9313-eb9bd8abb6f4" )]
