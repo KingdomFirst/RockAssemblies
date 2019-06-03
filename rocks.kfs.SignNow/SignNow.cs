846c77ee41fdec138e5147ab584283146f5aaa87
Trey Hendon <treyh@kingdomfirstsolutions.com>
2019-06-03 06:56:53 -0400

    Moved SignNow

--- rocks.kfs.SignNow/SignNow.cs
+++ rocks.kfs.SignNow/SignNow.cs
@@ -1,4 +1,28 @@
-﻿using System;
+﻿// <copyright>
+// Copyright by the Spark Development Network
+//
+// Licensed under the Rock Community License (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+// http://www.rockrms.com/license
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+// </copyright>
+//
+// <notice>
+// This file contains modifications by Kingdom First Solutions
+// and is a derivative work.
+//
+// Modification (including but not limited to):
+// * Adds ability to populate Smart Fields
+// </notice>
+//
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.ComponentModel.Composition;
@@ -18,7 +42,7 @@ using Rock.Model;
 using Rock.Security;
 using Rock.Web.Cache;
 
-namespace com.kfs.SignNow
+namespace rocks.kfs.SignNow
 {
     /// <summary>
     /// KFS SignNow Digital Signature Provider
