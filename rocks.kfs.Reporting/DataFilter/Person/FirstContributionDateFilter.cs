0d4d0e2ac1f5546e175f7629716ddc2a5af6f1a8
Trey Hendon <treyh@kingdomfirstsolutions.com>
2019-06-03 06:56:06 -0400

    Moved first contribution date dataview filter

--- rocks.kfs.Reporting/DataFilter/Person/FirstContributionDateFilter.cs
+++ rocks.kfs.Reporting/DataFilter/Person/FirstContributionDateFilter.cs
@@ -1,4 +1,28 @@
-﻿using System;
+﻿// <copyright>
+// Copyright by the Spark Development Network
+//
+// Licensed under the Rock Community License (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+// http://www.rockrms.com/license
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+// </copyright>
+//
+// <notice>
+// This file contains modifications by Kingdom First Solutions
+// and is a derivative work.
+//
+// Modification (including but not limited to):
+// * Adds ability to combine giving by household
+// </notice>
+//
+using System;
 using System.ComponentModel;
 using System.ComponentModel.Composition;
 using System.Linq;
@@ -10,7 +34,7 @@ using Rock.Model;
 using Rock.Reporting;
 using Rock.Web.UI.Controls;
 
-namespace com.kfs.Reporting.DataFilter.Person
+namespace rocks.kfs.Reporting.DataFilter.Person
 {
     /// <summary>
     /// 
