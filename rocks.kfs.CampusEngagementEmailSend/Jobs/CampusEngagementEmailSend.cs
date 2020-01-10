70cfa8be1e310e84e1afd50698629811e8ecd521
Dylan Gregory <dylang@kingdomfirstsolutions.com>
2020-01-10 13:54:13 -0500

    license change

--- rocks.kfs.CampusEngagementEmailSend/Jobs/CampusEngagementEmailSend.cs
+++ rocks.kfs.CampusEngagementEmailSend/Jobs/CampusEngagementEmailSend.cs
@@ -1,11 +1,11 @@
 ﻿// <copyright>
-// Copyright by the Spark Development Network
+// Copyright 2019 by Kingdom First Solutions
 //
-// Licensed under the Rock Community License (the "License");
+// Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
 // You may obtain a copy of the License at
 //
-// http://www.rockrms.com/license
+// http://www.apache.org/licenses/LICENSE-2.0
 //
 // Unless required by applicable law or agreed to in writing, software
 // distributed under the License is distributed on an "AS IS" BASIS,
@@ -13,14 +13,6 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 // </copyright>
-//
-// <notice>
-// This file contains modifications by Kingdom First Solutions
-// and is a derivative work.
-//
-// Modification (including but not limited to):
-// * Ability to email a specific role or staff member instead of leader.
-// </notice>
 //
 using System;
 using System.Collections.Generic;
