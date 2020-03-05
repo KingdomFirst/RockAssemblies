f889b57f86c2f8a30a07d6b28daaee131e92910d
Nate Hoffman <nateh@kingdomfirstsolutions.com>
2020-03-05 12:55:59 -0700

    Local setting change to use a group field instead of grouptypegroup field

--- rocks.kfs.FundraisingParticipantSummary/Jobs/FundraisingParticipantSummary.cs
+++ rocks.kfs.FundraisingParticipantSummary/Jobs/FundraisingParticipantSummary.cs
@@ -34,9 +34,9 @@ namespace rocks.kfs.FundraisingParticipantSummary.Jobs
     /// <summary>
     /// Job to send fundraising participant summary emails with donations since the last run of the job.
     /// </summary>
-    [SystemEmailField( "System Email", "The system email to use when sending the fundraising participant summary email.", true, "DE953A2C-1AE5-406D-B36C-B8486147D77B", "", 2 )]
-    [GroupTypesField( "Group Types", "The group types to send fundraising participant summary emails to.", false, "", "", 0 )]
-    [GroupTypeGroupField( "Group", "If you would like to narrow this job down to a specific group type/group to send the fundraising participant summary email to.", "Select a Group", false )]
+    [SystemEmailField( "System Email", "The system email to use when sending the fundraising participant summary email.", true, "DE953A2C-1AE5-406D-B36C-B8486147D77B", "" )]
+    [GroupTypesField( "Group Types", "The group types to send fundraising participant summary emails to.", false, "", "" )]
+    [GroupField( "Group", "If you would like to narrow this job down to a specific group type/group to send the fundraising participant summary email to.", false )]
     [BooleanField( "Show Address", "Determines if the Address column should be displayed in the Contributions List. (Sent to lava, has to be handled in lava display).", true )]
     [BooleanField( "Show Amount", "Determines if the Amount column should be displayed in the Contributions List. (Sent to lava, has to be handled in lava display).", true )]
     [BooleanField( "Send Emails with Zero Donations", "Should the emails to the group members still be sent if they had 0 donations in the time period? The time period for this job is anything since it last ran.", false )]
@@ -101,18 +101,7 @@ namespace rocks.kfs.FundraisingParticipantSummary.Jobs
                     groupTypes = new List<int>( groupTypeService.GetByGuids( selectedGroupTypes.Select( Guid.Parse ).ToList() ).Select( gt => gt.Id ) );
                 }
 
-                var parts = dataMap.Get( "Group" ).ToString().Split( '|' );
-
-                Guid? groupTypeGuid = null;
-                Guid? groupGuid = null;
-                if ( parts.Length >= 1 )
-                {
-                    groupTypeGuid = parts[0].AsGuidOrNull();
-                    if ( parts.Length >= 2 )
-                    {
-                        groupGuid = parts[1].AsGuidOrNull();
-                    }
-                }
+                var groupGuid = dataMap.Get( "Group" ).ToString().AsGuidOrNull();
 
                 if ( ( groupTypes.IsNull() || groupTypes.Count == 0 ) && !groupGuid.HasValue )
                 {
