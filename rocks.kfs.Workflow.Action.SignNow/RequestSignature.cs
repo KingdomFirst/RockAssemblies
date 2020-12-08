78dc58f7a4795f70345b4a56ad95d5591ed5e17c
Nate Hoffman <nateh@kingdomfirstsolutions.com>
2020-12-08 15:44:40 -0700

    Initialize new assembly for workflow action

---
+++ rocks.kfs.Workflow.Action.SignNow/RequestSignature.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace rocks.kfs.Workflow.Action.SignNow
+{
+    public class RequestSignature
+    {
+    }
+}
