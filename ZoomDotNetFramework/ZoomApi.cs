e5e3ec4e72605e8437d2eca761dd91b840127974
guy <guym@kingdomfirstsolutions.com>
2021-10-13 15:44:24 -0400

    Housekeeping

--- ZoomDotNetFramework/ZoomApi.cs
+++ ZoomDotNetFramework/ZoomApi.cs
@@ -24,7 +24,6 @@ namespace ZoomDotNetFramework
     using System.IdentityModel.Tokens;
     using System.Security.Claims;
     using Entities;
-    //using EventbriteDotNetFramework.Responses;
     using RestSharp;
     using JWT.Builder;
     using JWT.Algorithms;
@@ -35,44 +34,12 @@ namespace ZoomDotNetFramework
         private const string BaseUrl = "https://api.zoom.us/v2/";
         private string _jwtToken;
         private long _me;
-        private User _user;
-        //private long? _org;
 
-        //public long OrganizationId
-        //{
-        //    get
-        //    {
-        //        if ( !_org.HasValue )
-        //        {
-        //            var organizations = GetOrganizations();
-        //            if ( organizations != null && organizations.Organizations != null && organizations.Organizations.Any() )
-        //            {
-        //                _org = organizations.Organizations.Select( o => o.Id ).FirstOrDefault();
-        //            } 
-        //            else
-        //            {
-        //                return 0;
-        //            }
-        //        }
-        //        return _org.Value;
-        //    }
-        //    set
-        //    {
-        //        _org = value;
-        //    }
-        //}
         //Initializer
         public ZoomApi( string jwtToken )
         {
             _jwtToken = jwtToken;
         }
-        //public ZoomApi( string jwtToken, long orgId )
-        //{
-        //    _jwtToken = jwtToken;
-        //    _user = GetUser();
-        //    _me = _user.Id;
-        //    _org = orgId;
-        //}
 
         //Base Execute
         public T Execute<T>( RestRequest request ) where T : new()
@@ -118,169 +85,24 @@ namespace ZoomDotNetFramework
             return result.Rooms;
         }
 
-        public ZoomRoom GetZoomRoomById( string zoomRoomId )
+        public List<ZoomRoom> ScheduleZoomRoomMeeting( ZoomRoom room, string topic, string password )
         {
             var request = new RestRequest
             {
-                Resource = string.Format( "events/{0}/", id.ToString() )
+                Resource = string.Format( "rooms/{0}/meetings", room.Id )
             };
-
-            return Execute<Event>( request );
-        }
-
-        public EventOrders GetOrdersById( long id )
-        {
-            return GetOrdersById( id, 1 );
-        }
-
-        public EventOrders GetOrdersById( long id, int page )
-        {
-            var request = new RestRequest
+            if ( !string.IsNullOrWhiteSpace( password ) )
             {
-                Resource = string.Format( "events/{0}/orders/?page={1}", id.ToString(), page.ToString() )
-            };
+                request.AddParameter( "password", password );
+            }
+            var result = Execute<RoomsResponse>( request );
 
-            return Execute<EventOrders>( request );
-        }
-
-        public EventOrders GetExpandedOrdersById( long id )
-        {
-            return GetExpandedOrdersById( id, 1 );
-        }
-
-        public EventOrders GetExpandedOrdersById( long id, int page )
-        {
-            var request = new RestRequest
-            {
-                Resource = string.Format( "events/{0}/orders/?page={1}&expand=attendees", id.ToString(), page.ToString() )
-            };
-
-            return Execute<EventOrders>( request );
-        }
-        public Order GetOrderById( long id )
-        {
-            var request = new RestRequest
-            {
-                Resource = string.Format( "orders/{0}/", id.ToString() )
-            };
-
-            return Execute<Order>( request );
-        }
-        public Order GetExpandedOrderById( long id )
-        {
-            var request = new RestRequest
-            {
-                Resource = string.Format( "orders/{0}/?expand=attendees", id.ToString() )
-            };
-
-            return Execute<Order>( request );
-        }
-        public Order GetOrder( string apiUrl )
-        {
-            var request = new RestRequest
-            {
-                Resource = string.Format( "{0}", apiUrl.Replace( BaseUrl, "" ) )
-            };
-
-            return Execute<Order>( request );
-        }
-
-        public Order GetOrder( string apiUrl, string expand )
-        {
-            var request = new RestRequest
-            {
-                Resource = string.Format( "{0}?expand={1}", apiUrl.Replace( BaseUrl, "" ), expand )
-            };
-
-            return Execute<Order>( request );
-        }
-
-        public EventTicketClasses GetTicketsById( long id )
-        {
-            var request = new RestRequest
-            {
-                Resource = string.Format( "events/{0}/ticket_classes/", id.ToString() )
-            };
-
-            return Execute<EventTicketClasses>( request );
-        }
-
-        public EventCannedQuestions GetEventCannedQuestionsById( long id )
-        {
-            return GetEventCannedQuestionsbyId( id, 1 );
-        }
-
-        public EventCannedQuestions GetEventCannedQuestionsbyId( long id, int page )
-        {
-            var request = new RestRequest
-            {
-                Resource = string.Format( "events/{0}/canned_questions/?page={1}", id.ToString(), page.ToString() )
-            };
-
-            return Execute<EventCannedQuestions>( request );
-        }
-
-        public Attendee GetAttendee( string apiUrl )
-        {
-            var request = new RestRequest
-            {
-                Resource = string.Format( "{0}", apiUrl.Replace( BaseUrl, "" ) )
-            };
-
-            return Execute<Attendee>( request );
-        }
-
-        public Attendee GetAttendee( string apiUrl, string expand )
-        {
-            var request = new RestRequest
-            {
-                Resource = string.Format( "{0}?expand={1}", apiUrl.Replace( BaseUrl, "" ), expand )
-            };
-
-            return Execute<Attendee>( request );
-        }
-
-        public Attendee GetAttendee( long evntid, long id )
-        {
-            var request = new RestRequest
-            {
-                Resource = string.Format( "events/{0}/attendees/{1}", evntid.ToString(), id.ToString() )
-            };
-
-            return Execute<Attendee>( request );
+            return result.Rooms;
         }
 
         #endregion
 
         #region Mixed Methods
-
-        public OrganizationEventsResponse GetOrganizationEvents( bool RSVP = false )
-        {
-            var evnts = GetOrganizationEvents();
-            var retVar = new OrganizationEventsResponse
-            {
-                Pagination = evnts.Pagination,
-                Events = new List<Event>()
-            };
-            if ( evnts.Events != null )
-            {
-                foreach ( var evnt in evnts.Events )
-                {
-                    var cq = GetEventCannedQuestionsById( evnt.Id );
-                    var test = cq.Questions.FirstOrDefault( q => q.Respondent.Equals( "attendee", StringComparison.CurrentCultureIgnoreCase ) );
-                    if ( ( test == null && RSVP ) || ( test != null && !RSVP ) )
-                    {
-                        retVar.Events.Add( evnt );
-                    }
-                }
-            }
-            else
-            {
-                retVar.Events = evnts.Events;
-            }
-
-            return retVar;
-        }
 
         #endregion
 
