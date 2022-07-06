6aef6bc10061dc5d91dd4b662ae357bb7e2c3651
Nate Hoffman <nateh@kingdomfirstsolutions.com>
2022-07-06 14:59:26 -0600

    Handle Refund transaction fees as well.

--- rocks.kfs.Intacct/Properties/AssemblyInfo.cs
+++ rocks.kfs.Intacct/Properties/AssemblyInfo.cs
@@ -25,7 +25,7 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyCopyright( "Copyright © Kingdom First Solutions 2021" )]
 
 // Auto increment assembly versions
-[assembly: AssemblyVersion( "2.2.*" )]
+[assembly: AssemblyVersion( "2.3.*" )]
 
 // Setting ComVisible to false makes the types in this assembly not visible
 // to COM components.  If you need to access a type in this assembly from
