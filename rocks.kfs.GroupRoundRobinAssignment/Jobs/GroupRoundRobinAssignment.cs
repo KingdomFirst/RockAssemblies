6c17af33d55c9d854f725df5cd4d76f1defe0f62
Nate Hoffman <nateh@kingdomfirstsolutions.com>
2022-08-15 16:53:18 -0600

    Initialize empty job for Group Round Robin Assignment

---
+++ rocks.kfs.GroupRoundRobinAssignment/Jobs/GroupRoundRobinAssignment.cs
@@ -0,0 +1,74 @@
+﻿// <copyright>
+// Copyright 2022 by Kingdom First Solutions
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+// http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+// </copyright>
+//
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Web;
+using Quartz;
+using Rock;
+using Rock.Attribute;
+using Rock.Communication;
+using Rock.Data;
+using Rock.Model;
+using Rock.Web.Cache;
+
+namespace rocks.kfs.GroupRoundRobinAssignment.Jobs
+{
+    /// <summary>
+    /// Job to process Group Round Robin Assignments
+    /// </summary>
+    [DisallowConcurrentExecution]
+    public class GroupRoundRobinAssignment : IJob
+    {
+        private int errorCount = 0;
+        private List<string> errorMessages = new List<string>();
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="GroupRoundRobinAssignment"/> class.
+        /// </summary>
+        public GroupRoundRobinAssignment()
+        {
+        }
+
+        /// <summary>
+        /// Executes the specified context.
+        /// </summary>
+        /// <param name="context">The context.</param>
+        public virtual void Execute( IJobExecutionContext context )
+        {
+            JobDataMap dataMap = context.JobDetail.JobDataMap;
+
+            Guid? groupRoleGuid = dataMap.GetString( "GroupRole" ).AsGuidOrNull();
+
+            if ( errorMessages.Any() )
+            {
+                StringBuilder sb = new StringBuilder();
+                sb.AppendLine();
+                sb.Append( string.Format( "{0} Errors: ", errorCount ) );
+                errorMessages.ForEach( e => { sb.AppendLine(); sb.Append( e ); } );
+                string errors = sb.ToString();
+                context.Result += errors;
+                var exception = new Exception( errors );
+                HttpContext context2 = HttpContext.Current;
+                ExceptionLogService.LogException( exception, context2 );
+                throw exception;
+            }
+        }
+    }
+}
