e24be1f4f8e025d692c59242a3be80fc03d3c6af
Nate H <nateh777@users.noreply.github.com>
2022-08-18 15:24:48 -0600

    Merge pull request #86 from KingdomFirst/feature/CustomGroupReminderEmails
    Custom Group Reminder Emails
    
    (cherry picked from commit 70c81b4a10af74e2faf6382a7439df62d987bfdd)

---
+++ rocks.kfs.CustomGroupCommunication/Guids/SystemCommunication.cs
@@ -0,0 +1,26 @@
+﻿// <copyright>
+// Copyright 2022 by Kingdom First Solutions
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+// http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+// </copyright>
+//
+namespace rocks.kfs.CustomGroupCommunication.Guid
+{
+    public static class SystemComunication
+    {
+        /// <summary>
+        /// Zoom Meeting Notification System Communication
+        /// </summary>
+        public const string CUSTOM_GROUP_MEETING_REMINDER = "F2B18235-1BA3-421D-A62B-1713B38B7112";
+    }
+}
