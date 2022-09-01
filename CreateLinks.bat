4bdd4d64e1f3d094798dd6a2942953649f8b76c9
Nate Hoffman <nateh@kingdomfirstsolutions.com>
2022-08-31 18:08:50 -0600

    v14 SLN Changes, bring in a batch file to help setup symlinks

---
+++ CreateLinks.bat
@@ -0,0 +1,5 @@
+mklink /J KFSRockAssemblies c:\KFSRepo\Rock\KFSRockAssemblies
+mklink /J RockAttendedCheckin c:\KFSRepo\Rock\RockAttendedCheckin 
+mklink /J RockWeb\Plugins\rocks_kfs c:\KFSRepo\Rock\KFSRockBlocks
+mklink /J RockWeb\Plugins\cc_newspring c:\KFSRepo\Rock\RockAttendedCheckin\cc_newspring
+mklink /J RockWeb\Content\KFSRockAssets c:\KFSRepo\Rock\KFSRockAssets
