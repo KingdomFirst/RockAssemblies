c8015c099f715aed6ac70e57f1c3e474602555f3
Nate H <nateh777@users.noreply.github.com>
2023-01-31 09:51:24 -0700

    Cherry pick merge conflict resolution.
    (cherry picked from commit 568b6d610f4556bf4ae835d30efeb886753c9886)

--- rocks.kfs.Microsoft365Utilities/Migrations/001_CreateEWSAttributes.cs
+++ rocks.kfs.Microsoft365Utilities/Migrations/001_CreateEWSAttributes.cs
@@ -20,15 +20,15 @@ using Rock.Plugin;
 
 namespace rocks.kfs.Shortcodes.Migrations
 {
-    [MigrationNumber( 2, "1.12.6" )]
-    public class CreateAzureAppAttributes : Migration
+    [MigrationNumber( 1, "1.12.8" )]
+    public class CreateEWSAttributes : Migration
     {
         public override void Up()
         {
             RockMigrationHelper.AddGlobalAttribute( Rock.SystemGuid.FieldType.ENCRYPTED_TEXT, null, null, "EWS Azure Application ID", "The Application (client) ID in Microsoft Azure for the registered application to use for accessing the EWS managed API. Value should be in the format of a guid.", 0, null, "467C0F95-BAB9-49E9-B675-6AD59EB221D8", "rocks.kfs.EWSAppApplicationId", true );
             RockMigrationHelper.AddGlobalAttribute( Rock.SystemGuid.FieldType.ENCRYPTED_TEXT, null, null, "EWS Azure Tenant ID", "The Directory (tenant) ID in Microsoft Azure for the registered application to use for accessing the EWS managed API. Value should be in the format of a guid.", 0, null, "12D2D842-8806-402D-BCD7-E9E81D9FDEAB", "rocks.kfs.EWSAppTenantId", true );
             RockMigrationHelper.AddGlobalAttribute( Rock.SystemGuid.FieldType.ENCRYPTED_TEXT, null, null, "EWS Azure Secret", "The Secret Value in Microsoft Azure for the registered application to use for accessing the EWS managed API.", 0, null, "8CDE7476-506B-4F39-B2BE-5E4102F3C9F3", "rocks.kfs.EWSAppSecret", true );
-            RockMigrationHelper.AddAttributeQualifier( "8CDE7476-506B-4F39-B2BE-5E4102F3C9F3","ispassword","true", "B70BC64F-3DCD-402C-A24D-38A8CD0428BD" );
+            RockMigrationHelper.AddAttributeQualifier( "8CDE7476-506B-4F39-B2BE-5E4102F3C9F3", "ispassword", "true", "B70BC64F-3DCD-402C-A24D-38A8CD0428BD" );
         }
 
         public override void Down()
