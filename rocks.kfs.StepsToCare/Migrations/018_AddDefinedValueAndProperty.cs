5b567ac117a4767fa5d235516c1d25c47892e5d2
Nate Hoffman <nateh@kingdomfirstsolutions.com>
2024-04-02 18:21:28 -0600

    Cleanup a few minor changes found in merge review.

--- rocks.kfs.StepsToCare/Migrations/018_AddDefinedValueAndProperty.cs
+++ rocks.kfs.StepsToCare/Migrations/018_AddDefinedValueAndProperty.cs
@@ -14,7 +14,6 @@
 // limitations under the License.
 // </copyright>
 //
-using Rock;
 using Rock.Plugin;
 
 namespace rocks.kfs.StepsToCare.Migrations
@@ -26,8 +25,8 @@ namespace rocks.kfs.StepsToCare.Migrations
         {
             RockMigrationHelper.UpdateDefinedValue( SystemGuid.DefinedType.CARE_NEED_STATUS, "Snoozed", "", SystemGuid.DefinedValue.CARE_NEED_STATUS_SNOOZED, true );
             RockMigrationHelper.AddDefinedValueAttributeValue( SystemGuid.DefinedValue.CARE_NEED_STATUS_SNOOZED, "ACD00438-3E25-4E17-8875-742C27EC7B30", @"label label-primary" );
+            RockMigrationHelper.AddDefinedTypeAttribute( SystemGuid.DefinedType.CARE_NEED_CATEGORY, "A75DFC58-7A1B-4799-BF31-451B2BBE38FF", "Follow Up After", "FollowUpAfter", "Will repeat the notification and change to follow up status the provided number of days after the status has changed to \"Snoozed\". If you have a value set in this attribute, by default when this category is selected it will enable \"Custom Follow Up\" and set the Follow Up After value to this on the Care Need.", 6563, "", "CF59CDFF-8C98-487B-B2E7-A2CA496ACB87" );
             RockMigrationHelper.AddDefinedTypeAttribute( SystemGuid.DefinedType.CARE_NEED_CATEGORY, "A75DFC58-7A1B-4799-BF31-451B2BBE38FF", "Number of Times to Repeat", "TimesToRepeat", "The number of times to repeat.  Leave blank to repeat indefinitely. Only applicable if Follow Up After is set.", 6564, "", "39CF47AD-8A04-41D2-B629-D4E3822D4A1D" );
-            RockMigrationHelper.AddDefinedTypeAttribute( SystemGuid.DefinedType.CARE_NEED_CATEGORY, "A75DFC58-7A1B-4799-BF31-451B2BBE38FF", "Follow Up After", "FollowUpAfter", "Will repeat the notification and change to follow up status the provided number of days after the status has changed to \"Waiting\". If you have a value set in this attribute, by default when this category is selected it will enable \"Custom Follow Up\" and set the Follow Up After value to this on the Care Need.", 6563, "", "CF59CDFF-8C98-487B-B2E7-A2CA496ACB87" );
             Sql( @"IF COL_LENGTH('_rocks_kfs_StepsToCare_CareNeed', 'CustomFollowUp') IS NULL
                 BEGIN
 	                ALTER TABLE _rocks_kfs_StepsToCare_CareNeed ADD [CustomFollowUp] bit NOT NULL DEFAULT (0), [RenewPeriodDays] int NULL, [RenewMaxCount] int NULL, [RenewCurrentCount] int NOT NULL DEFAULT (0), [SnoozeDate] [datetime] NULL
