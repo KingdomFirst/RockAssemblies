6ecbae981df0b7e4bbac04794c59e11a6853cd28
Guy Maness <guy.maness@gmail.com>
2024-05-07 14:29:55 -0400

    Merge pull request #147 from KingdomFirst/feature/nbh-StepsToCareUpdates
    Steps to Care - Minor Fixes
    
    (cherry picked from commit 07a38bc717bb95126d17104f67bd17d87ecd2fc7)

--- rocks.kfs.Intacct/Properties/AssemblyInfo.cs
+++ rocks.kfs.Intacct/Properties/AssemblyInfo.cs
@@ -1,5 +1,5 @@
 ﻿// <copyright>
-// Copyright 2023 by Kingdom First Solutions
+// Copyright 2024 by Kingdom First Solutions
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
@@ -25,7 +25,7 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyCopyright( "Copyright © Kingdom First Solutions 2024" )]
 
 // Auto increment assembly versions
-[assembly: AssemblyVersion( "2.8.1.*" )]
+[assembly: AssemblyVersion( "2.8.2.*" )]
 
 // Setting ComVisible to false makes the types in this assembly not visible
 // to COM components.  If you need to access a type in this assembly from
