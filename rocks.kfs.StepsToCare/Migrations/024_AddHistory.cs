33eefa9f46822735b6ed1a50974c0de4539b8054
Nate Hoffman <nateh@kingdomfirstsolutions.com>
2025-05-21 14:49:00 -0600

    Add new migration to create a history page with history log block and setup some categories for history.

---
+++ rocks.kfs.StepsToCare/Migrations/024_AddHistory.cs
@@ -0,0 +1,63 @@
+﻿// <copyright>
+// Copyright 2025 by Kingdom First Solutions
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+// http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+// </copyright>
+//
+using System;
+using Rock;
+using Rock.Data;
+using Rock.Model;
+using Rock.Plugin;
+
+namespace rocks.kfs.StepsToCare.Migrations
+{
+    [MigrationNumber( 24, "1.16.0" )]
+    public class AddHistory : Migration
+    {
+        public override void Up()
+        {
+            RockMigrationHelper.UpdateCategory( Rock.SystemGuid.EntityType.HISTORY, "Steps to Care - Care Need", "fa fa-hand-holding", "History entries for the plugin Steps to Care Care Need entity.", SystemGuid.Category.HISTORY_CARE_NEED );
+            RockMigrationHelper.UpdateCategory( Rock.SystemGuid.EntityType.HISTORY, "Steps to Care", "fa fa-hand-holding", "History entries for the plugin Steps to Care under Person history (assign workers or other history directly related to a person).", SystemGuid.Category.HISTORY_PERSON_STEPS_TO_CARE, 0, Rock.SystemGuid.Category.HISTORY_PERSON );
+
+            RockMigrationHelper.AddPage( true, "27953B65-21E2-4CA9-8461-3AFAD46D9BC8", "D65F783D-87A9-4CC9-8110-E83466A0EADB", "Care Need History", "", "653F2B13-E828-4263-B12B-C419A1833197" );
+            RockMigrationHelper.AddBlock( "653F2B13-E828-4263-B12B-C419A1833197", "", "C6C2DF41-A50D-4975-B21C-4EFD6FF3E8D0", "History Log", "Main", "", "", 0, "C5E4F1D7-0B2A-4F3C-8D6E-9B5A0F1D7E3F" );
+
+            //   Attribute: Heading
+            RockMigrationHelper.AddBlockAttributeValue( "C5E4F1D7-0B2A-4F3C-8D6E-9B5A0F1D7E3F", "EAAE646D-69CD-41AC-9B8A-7EC5A446B379", @"{{ Entity.EntityStringValue }} (ID:{{ Entity.Id }})" );
+            //   Attribute: Entity Type
+            RockMigrationHelper.AddBlockAttributeValue( "C5E4F1D7-0B2A-4F3C-8D6E-9B5A0F1D7E3F", "E9BB2534-D54E-4D50-A241-FCD45EFADE32", @"87ac878d-6740-43eb-9389-b8440ac595c3" );
+            //   Attribute: Category
+            RockMigrationHelper.AddBlockAttributeValue( "C5E4F1D7-0B2A-4F3C-8D6E-9B5A0F1D7E3F", "44092D4B-213D-4572-A005-C2B35E0B4082", @"4c4b37d6-6966-4864-ab06-ff92ece501bb" );
+
+            RockMigrationHelper.UpdatePageContext( "653F2B13-E828-4263-B12B-C419A1833197", "rocks.kfs.StepsToCare.Model.CareNeed", "CareNeedId", "81EE561B-C87F-46DE-AAF4-8B6476E2AA79" );
+            RockMigrationHelper.AddOrUpdatePageRoute( "653F2B13-E828-4263-B12B-C419A1833197", "StepsToCareDetail/{CareNeedId}/History", "73EDDBA6-737D-423F-B513-6E08113DA346" );
+
+            RockMigrationHelper.AddOrUpdateBlockTypeAttribute( "4F0F9ED7-9F74-4152-B27F-D9B2A458AFBE", "BD53F9C9-EBA9-4D3F-82EA-DE5DD34A8108", "Care Need History Page", "CareNeedHistoryPage", "Care Need History Page", @"Page used to display history details.", 0, @"", "9C990D50-769F-4BFD-8AFE-02092C76B68C" );
+            RockMigrationHelper.AddBlockAttributeValue( "F953C5EF-6504-45F9-81A8-063518B7AB61", "9C990D50-769F-4BFD-8AFE-02092C76B68C", @"653f2b13-e828-4263-b12b-c419a1833197,73eddba6-737d-423f-b513-6e08113da346" );
+        }
+
+        public override void Down()
+        {
+            RockMigrationHelper.DeleteCategory( SystemGuid.Category.HISTORY_CARE_NEED );
+            RockMigrationHelper.DeleteCategory( SystemGuid.Category.HISTORY_PERSON_STEPS_TO_CARE );
+
+            RockMigrationHelper.DeletePageContext( "81EE561B-C87F-46DE-AAF4-8B6476E2AA79" );
+            RockMigrationHelper.DeletePageRoute( "73EDDBA6-737D-423F-B513-6E08113DA346" );
+            RockMigrationHelper.DeleteBlock( "C5E4F1D7-0B2A-4F3C-8D6E-9B5A0F1D7E3F" );
+            RockMigrationHelper.DeletePage( "653F2B13-E828-4263-B12B-C419A1833197" );
+
+            RockMigrationHelper.DeleteAttribute( "9C990D50-769F-4BFD-8AFE-02092C76B68C" );
+        }
+    }
+}
