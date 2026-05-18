454cc0b842493cb125a369722a22d38d0fa8846d
guymaness <guym@kingdomfirstsolutions.com>
2026-05-18 16:50:41 -0400

    Could have sworn this worked, but doesn't

--- rocks.kfs.Intacct/IntacctOtherReceipt.cs
+++ rocks.kfs.Intacct/IntacctOtherReceipt.cs
@@ -172,18 +172,19 @@ namespace rocks.kfs.Intacct
                                 writer.WriteElementString( "classid", item.ClassId );
                             }
 
-                            if ( item.CustomFields.Count > 0 )
-                            {
-                                writer.WriteStartElement( "customfields" );
-                                foreach ( KeyValuePair<string, dynamic> customField in item.CustomFields )
-                                {
-                                    writer.WriteStartElement( "customfield" );
-                                    writer.WriteElementString( "customfieldname", customField.Key );
-                                    writer.WriteElementString( "customfieldvalue", customField.Value ?? string.Empty );
-                                    writer.WriteEndElement();  // close customfield
-                                }
-                                writer.WriteEndElement();  // close customfields
-                            }
+                            // Intacct XML api documentation shows support for custom fields, but we are unable to get them to work. Disabling for now.
+                            //if ( item.CustomFields.Count > 0 )
+                            //{
+                            //    writer.WriteStartElement( "customfields" );
+                            //    foreach ( KeyValuePair<string, dynamic> customField in item.CustomFields )
+                            //    {
+                            //        writer.WriteStartElement( "customfield" );
+                            //        writer.WriteElementString( "customfieldname", customField.Key );
+                            //        writer.WriteElementString( "customfieldvalue", customField.Value ?? string.Empty );
+                            //        writer.WriteEndElement();  // close customfield
+                            //    }
+                            //    writer.WriteEndElement();  // close customfields
+                            //}
                             writer.WriteEndElement();  // close lineitem
                         }
 
